b638c88afb4a380dffa1b7bb77703f6e3d84e2f3
Mark Mitzen <mmitzen@nevada.unr.edu>
2024-06-15 04:06:25 -0700

    idk

--- customer.py
+++ customer.py
@@ -18,7 +18,7 @@ class Customer:
         if account_index not in self.accounts:
             print("Invalid account index")
             return False
-        self.accounts[account_index].deposit()
+        self.accounts[account_index].deposit(amount)
         return True
 
     def credit_account(self, account_index, amount):
@@ -31,8 +31,18 @@ class Customer:
         self.accounts[account_index].withdraw(amount)
         if self.accounts[account_index].type == "Savings":
             self.savings_withdraw_count += 1
+        if self.savings_withdraw_count >= 6:
+            self.savings_deduction(self, account_index)
 
     def list_accounts(self):
         print(f"Accounts for {self.name}:")
         for i in range(len(self.accounts)):
             print(i, ". ", str(self.accounts[i]))
+
+    def savings_deduction(self, account_index):
+        self.accounts[account_index].withdraw(10)
+        self.savings_withdraw_count = 0
+        if self.accounts[account_index].balance < 0:
+            print("Account closure or something lol")
+            return False
+        return True
--- main.py
+++ main.py
@@ -3,6 +3,18 @@ from bankAccount import BankAccount
 
 print("Hello World")
 james = Customer("James")
-account = BankAccount("Checking", 12.50)
-james.add_account(account)
+charlie = Customer("Charlie")
+jenny = Customer("Jenny")
+cassandra = Customer("Cassandra")
+checking1 = BankAccount("Checking", 12.50)
+checking2 = BankAccount("Checking", 5600)
+checking3 = BankAccount("Checking", 0)
+savings1 = BankAccount("Savings", 300)
+savings2 = BankAccount("Savings", 900000)
+savings3 = BankAccount("Savings", 20)
+
+james.add_account(checking1)
+james.add_account(savings1)
+jenny.add_account(checking2)
+
 james.list_accounts()
